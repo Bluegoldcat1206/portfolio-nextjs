ddcc2bdfcc06b3d5ed369af78c3b12844225ac59
Bluegoldcat1206 <Nataliemb1206@gmail.com>
2026-05-06 11:44:29 -0700

    checking analytics

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: "The Unhinged Creative",
@@ -31,6 +32,7 @@ export default function RootLayout({
 
         {children}
       </body>
+      <GoogleAnalytics gaId="G-9WTTPWKGYS" />
     </html>
   );
 }
