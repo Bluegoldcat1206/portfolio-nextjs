e3b87d80e59d85264b9a037b0e1e6030c1179f9b
Bluegoldcat1206 <Nataliemb1206@gmail.com>
2026-05-06 11:26:34 -0700

    Dynamic project pages

--- nextjs/app/portfolio/[slug]/page.tsx
+++ nextjs/app/portfolio/[slug]/page.tsx
@@ -3,6 +3,7 @@ import projects from "../../data/projects"
 export default async function ProjectPage({ params }) {
   const { slug } = await params;
   const project = projects.find((project) => project.slug === slug)
+  console.log("project:", project)
 
   if (!project) {
     return <h1>Project not found</h1>
@@ -11,6 +12,7 @@ export default async function ProjectPage({ params }) {
   return (
     <main>
       <h1>{project.title}</h1>
+      <p>{project.year}</p>
       <img src={project.images[0].url} alt={project.images[0].alt} />
       <p>{project.description}</p>
       <div>
