4a4ba2d22f30c7dbafb215175881b45bc5bbfe84
Bluegoldcat1206 <Nataliemb1206@gmail.com>
2026-05-11 10:45:42 -0700

    robots file

---
+++ nextjs/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://your-vercel-url.vercel.app/sitemap.xml',
+  }
+}
